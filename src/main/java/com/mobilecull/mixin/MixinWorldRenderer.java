30dc1eef6d610163e5d250939d514fbe2e715d71
alae2011n <pawnmadara@gmail.com>
2026-06-17 23:56:08 +0100

    Update MixinWorldRenderer.java

--- src/main/java/com/mobilecull/mixin/MixinWorldRenderer.java
+++ src/main/java/com/mobilecull/mixin/MixinWorldRenderer.java
@@ -5,14 +5,14 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.RenderTickCounter;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = WorldRenderer.class, priority = 900)
+@Mixin(value = WorldRenderer.class, priority = 999)
 public class MixinWorldRenderer {
 
     @Inject(
@@ -20,14 +20,11 @@ public class MixinWorldRenderer {
         at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Frustum;setPosition(DDD)V")
     )
     private void captureFrustumMatrices(
-        MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline,
-        Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager,
-        Matrix4f projectionMatrix, CallbackInfo ci
+        RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, 
+        GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, 
+        Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci
     ) {
-        Matrix4f modelViewMatrix = matrices.peek().getPositionMatrix();
-        Matrix4f combinedMvp = new Matrix4f(projectionMatrix).mul(modelViewMatrix);
-        
+        Matrix4f combinedMvp = new Matrix4f(projectionMatrix).mul(positionMatrix);
         MobileCullEngine.INSTANCE.updateFrustum(combinedMvp);
     }
 }
-
