757aa673f48dca9581486633a56f833e610fb4f3
alae2011n <pawnmadara@gmail.com>
2026-06-18 00:11:43 +0100

    Update MixinParticle.java

--- src/main/java/com/mobilecull/mixin/MixinParticle.java
+++ src/main/java/com/mobilecull/mixin/MixinParticle.java
@@ -2,33 +2,24 @@ package com.mobilecull.mixin;
 
 import com.mobilecull.engine.MobileCullEngine;
 import net.minecraft.client.particle.Particle;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.VertexConsumer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Particle.class)
-public abstract class MixinParticle {
-    @Shadow protected double x;
-    @Shadow protected double y;
-    @Shadow protected double z;
+public class MixinParticle {
 
-    @Inject(method = "buildGeometry", at = @At("HEAD"), cancellable = true)
-    private void cullParticleRender(VertexConsumer vertexConsumer, Camera camera, float tickDelta, CallbackInfo ci) {
-        MobileCullEngine engine = MobileCullEngine.INSTANCE;
-        if (engine.cullParticles) {
-            // Check visibility using a tightly bound bounding point for lightning-fast lookups
-            boolean visible = engine.isBoxVisible(
-                this.x - 0.1, this.y - 0.1, this.z - 0.1,
-                this.x + 0.1, this.y + 0.1, this.z + 0.1
-            );
-            if (!visible) {
-                ci.cancel();
-            }
+    @Inject(
+        method = "buildGeometry",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void onBuildGeometry(VertexConsumer vertexConsumer, Camera camera, float tickDelta, CallbackInfo ci) {
+        if (!MobileCullEngine.INSTANCE.shouldRender((Particle) (Object) this)) {
+            ci.cancel();
         }
     }
 }
-
