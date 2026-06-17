0d951f6e6ac007543f468aea3643d968bfe9b353
alae2011n <pawnmadara@gmail.com>
2026-06-18 00:03:08 +0100

    Update MixinEntityRenderDispatcher.java

--- src/main/java/com/mobilecull/mixin/MixinEntityRenderDispatcher.java
+++ src/main/java/com/mobilecull/mixin/MixinEntityRenderDispatcher.java
@@ -5,7 +5,6 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,24 +13,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderDispatcher.class)
 public class MixinEntityRenderDispatcher {
 
-    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    private <E extends Entity> void cullEntityRender(
-        E entity, double x, double y, double z, float yaw, float tickDelta,
+    @Inject(
+        method = "render",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private <E extends Entity> void onRender(
+        E entity, double x, double y, double z, float yaw, float tickDelta, 
         MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci
     ) {
-        MobileCullEngine engine = MobileCullEngine.INSTANCE;
-        if (engine.cullEntities) {
-            Box box = entity.getBoundingBox();
-            
-            boolean visible = engine.isBoxVisible(
-                box.minX, box.minY, box.minZ,
-                box.maxX, box.maxY, box.maxZ
-            );
-
-            if (!visible) {
-                ci.cancel();
-            }
+        if (!MobileCullEngine.INSTANCE.shouldRender(entity)) {
+            ci.cancel();
         }
     }
-  }
-              
+}
